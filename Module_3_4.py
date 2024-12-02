e2c3a20c96f004af0a9c151f6e590e80eca62541
Даниил <daniil.lebedinsky@gmail.com>
2024-12-02 19:13:19 +0300

    ДЗ  №4

--- Module_3_4.py
+++ Module_3_4.py
@@ -1,9 +1,11 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     for i in other_words:
-        if root_word in i or i in root_word:
+        if root_word.lower().upper() in i.lower().upper() or i.lower().upper() in root_word.lower().upper():
             same_words.append(i)
     return same_words
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 print(result1)
+result2 = single_root_words('Disablement', 'Able', 'able', 'AbLe')
+print(result2)
