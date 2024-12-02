64d0f7cd76915face6138e2f97422e0aaa0966a1
Даниил <daniil.lebedinsky@gmail.com>
2024-12-02 19:26:53 +0300

    ДЗ  №2

--- Module_3_2.py
+++ Module_3_2.py
@@ -1,7 +1,7 @@
 def send_email(message, recipient, sender =  "university.help@gmail.com"):
-    if '@' not in recipient or not (recipient.endswith('.ru') or recipient.endswith('.com') or recipient.endswith('.net')):
+    if '@' not in recipient or not (recipient.endswith('.ru' or '.com' or '.net')):
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
-    elif '@' not in sender or not (sender.endswith('.ru') or sender.endswith('.com') or sender.endswith('.net')):
+    elif '@' not in sender or not (sender.endswith('.ru' or '.com' or '.net')):
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
     elif sender == recipient:
         print('Нельзя отправить письмо самому себе!')
