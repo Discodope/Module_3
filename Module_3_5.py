7f6beb0b73d32cfee43b6e0601ea588651004fcf
Даниил <daniil.lebedinsky@gmail.com>
2024-12-02 20:33:16 +0300

    ДЗ  №5

--- Module_3_5.py
+++ Module_3_5.py
@@ -3,10 +3,12 @@ def get_multiplied_digits(number):
     if len(str_number) > 1:
         first = int(str_number[0])
         return first * get_multiplied_digits(int(str_number[1:]))
-    elif len(str_number) == 0:
+    if len(str_number) == 0:
         return 1
+    for j in str_number:
+        if j == '0':
+            return 1
     else:
         return int(str_number)
-
-res1 = get_multiplied_digits(40203)
+res1 = get_multiplied_digits(402030)
 print(res1)
